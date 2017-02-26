21b230ea572819f53ed6b8969d2ab3bef9dcfcf9
MARCLAPTOP\mboye <mboyer5797@gmail.com>
2017-02-26 02:33:08 -0500

    basic psuedocode into java

--- Recursion.java
+++ Recursion.java
@@ -1,18 +1,23 @@
 public class Recursion{
-  
-  public static double squareRoot(x, guess, error){
+    
+  //double guess = x / 2;
+    
+  public static double squareRoot(double x, double guess, double error){
+    double acceptedError = 0.25;
     error = x - guess * guess;
     if (error <= acceptedError)
       return guess;
     else {
-      newGuess = 0.5 * (lastGuess + x / lastGuess);
-      squareRoot(x, newGuess, error);
+      double newGuess = 0.5 * ((guess + x) / guess);
+      return squareRoot(x, newGuess, error);
     }
   }
+  
+  
+  public static void main(String[] args){
+        System.out.println(squareRoot(9, 4.5));
+  
+  }
     
-  
-  
-  
-  
-  
+
 }
---
+++ Recursion.java~
@@ -0,0 +1,18 @@
+public class Recursion{
+  
+  public static double squareRoot(x, guess, error){
+    error = x - guess * guess;
+    if (error <= acceptedError)
+      return guess;
+    else {
+      newGuess = 0.5 * (lastGuess + x / lastGuess);
+      squareRoot(x, newGuess, error);
+    }
+  }
+    
+  
+  
+  
+  
+  
+}
