a1c3ad32855decfda897608555d1f18d2ceaebee
MARCLAPTOP\mboye <mboyer5797@gmail.com>
2017-02-26 22:20:11 -0500

    getting ready for debugging

--- Recursion.java
+++ Recursion.java
@@ -1,21 +1,29 @@
-public class Recursion{
+public class Recursion{ 
+  
+/*  public static double root (double x, double acceptedError){
     
-  //double guess = x / 2;
+    double guess = x / 2;
+    double error = x - guess * guess;
+    acceptedError = acceptedError;
+    return squareRoot(x, guess, error);
     
-  public static double squareRoot(double x, double guess, double error){
-    double acceptedError = 0.25;
+  }*/
+  
+  public static double squareRoot(double x, double guess, double error, double acceptedError){
     error = x - guess * guess;
     if (error <= acceptedError)
       return guess;
     else {
-      double newGuess = 0.5 * ((guess + x) / guess);
-      return squareRoot(x, newGuess, error);
+      double newGuess = 0.5 * (guess + x / guess);
+      return squareRoot(x, newGuess, error, acceptedError);
     }
   }
   
   
   public static void main(String[] args){
-        System.out.println(squareRoot(9, 4.5));
+    double x = 9;
+    double acceptedError = 0.25;
+    System.out.println(squareRoot(x, x/2, 1, acceptedError));
   
   }
     
